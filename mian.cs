98c1ff7810e87c4c5787e9259f3551b83f6c58a7
Edson <44821520+edyi13@users.noreply.github.com>
2022-07-06 02:03:53 +0000

    changes in mian.cs

--- mian.cs
+++ mian.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 
 class Program {
+  
   public static void findNemo(string[] arr){
     // Stopwatch sw = Stopwatch.StartNew();
     for(int i = 0; i < arr.Length;i++){
@@ -12,6 +13,13 @@ class Program {
     // sw.Stop();
     // Console.WriteLine("Finding nemo takes " + sw.Elapsed.TotalMilliseconds);
   }
+
+  public static void logFirst2Boxes(int[] boxes){
+     Console.WriteLine(boxes[0]);
+     Console.WriteLine(boxes[1]);
+  }
+  
+  
   public static void Main (string[] args) {
     string[] nemo = new string[] {"nemo"};
     string[] fish = new string[] {"dory", "bruce", "marlin", "nemo"};
@@ -19,6 +27,10 @@ class Program {
 
     string[] large = new string [10000];
     Array.Fill(large,"nemo");
-    findNemo(large); //Big O notation = 0(n) --> Linear Time
+    //findNemo(large); //Big O notation = 0(n) --> Linear Time
+    /************************************************************/
+
+    int[] arr = new int[]{1,2,3,4,5};
+    logFirst2Boxes(arr); //Big O notation 0(1) --> Constant Time
   }
 }
