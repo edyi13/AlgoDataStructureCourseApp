1689cb163f6f023c0a3894a77c5fe55606db3eb3
Edson <44821520+edyi13@users.noreply.github.com>
2022-07-06 00:09:54 +0000

    change the file name and add some arrays for testing

---
+++ findNemo.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Diagnostics;
+
+class Program {
+  public static void findNemo(string[] arr){
+    Stopwatch sw = Stopwatch.StartNew();
+    for(int i = 0; i < arr.Length;i++){
+      if(arr[i] == "nemo"){
+        Console.WriteLine("Found Nemo!!!");
+      }      
+    }
+    sw.Stop();
+    Console.WriteLine("Finding nemo takes " + sw.Elapsed.TotalMilliseconds);
+  }
+  public static void Main (string[] args) {
+    string[] nemo = new string[] {"nemo"};
+    string[] fish = new string[] {"dory", "bruce", "marlin", "nemo"};
+    string[] everyone = new string[] {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
+
+    string[] large = new string [10000];
+    Array.Fill(large,"nemo");
+    findNemo(large);
+  }
+}
