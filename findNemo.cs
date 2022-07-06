dcd3c6683d1f942aded3fc0d0abfae971c1ee920
Edson <44821520+edyi13@users.noreply.github.com>
2022-07-06 00:21:18 +0000

    comments on some code lines

--- findNemo.cs
+++ findNemo.cs
@@ -3,22 +3,22 @@ using System.Diagnostics;
 
 class Program {
   public static void findNemo(string[] arr){
-    Stopwatch sw = Stopwatch.StartNew();
+    // Stopwatch sw = Stopwatch.StartNew();
     for(int i = 0; i < arr.Length;i++){
       if(arr[i] == "nemo"){
         Console.WriteLine("Found Nemo!!!");
       }      
     }
-    sw.Stop();
-    Console.WriteLine("Finding nemo takes " + sw.Elapsed.TotalMilliseconds);
+    // sw.Stop();
+    // Console.WriteLine("Finding nemo takes " + sw.Elapsed.TotalMilliseconds);
   }
   public static void Main (string[] args) {
     string[] nemo = new string[] {"nemo"};
     string[] fish = new string[] {"dory", "bruce", "marlin", "nemo"};
-    string[] everyone = new string[] {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
+    string[] everyone = new string[] {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt","darla", "hank"};
 
     string[] large = new string [10000];
     Array.Fill(large,"nemo");
-    findNemo(large);
+    findNemo(large); //Big O notation = 0(n) --> Linear Time
   }
 }
